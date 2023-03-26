1523006ccab14203c43be6fe7d455943de47c9f9
evgeniac10 <rlagustjd2684@gmail.com>
2023-03-26 20:50:23 +0900

    Update 스택수열_김현성.java
    스택을 이용한 알고리즘

--- 2주차/스택 수열/스택수열_김현성.java
+++ 2주차/스택 수열/스택수열_김현성.java
@@ -1,13 +1,15 @@
 package 백준;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class 백준_스택수열_1874번 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
 		Stack<Integer> st = new Stack();
 		int T = Integer.parseInt(br.readLine());
 		int stack_num = 1; // 스택에 오름차순으로 차례대로 숫자 넣기 위해 만듬
@@ -19,20 +21,18 @@ public class 백준_스택수열_1874번 {
 			if (stack_num <= num) {// 입력받은 숫자까지만큼 스택에 숫자 넣어서 쌓기
 				for (int j = stack_num; j <= num; j++) {
 					st.push(j);
-					bw.write("+" + "\n");
+					sb.append("+" + "\n");
 				}
 				stack_num = num + 1;
 			} else if (st.peek() != num) {// 스택에 쌓인 값중에 가장 먼저 나올수있는 값과 입력받은 숫자와 비교
-				bw = new BufferedWriter(new OutputStreamWriter(System.out)); //만들수 없는 수열이므로 버퍼비우기 
-				bw.write("NO");
-				break;// peek()와 pop()의 차이 값을 꺼내고 삭제 하냐 안하냐
+				 System.out.println("NO");//만들수 없는 수열이므로 버퍼비우기 
+				return;
+				// peek()와 pop()의 차이 값을 꺼내고 삭제 하냐 안하냐
 			}
 			st.pop();
-			bw.write("-" + "\n");
+			sb.append("-" + "\n");
 		}
-		bw.flush();
+		System.out.println(sb);
 		br.close();
-		bw.close();
 	}
-
 }
