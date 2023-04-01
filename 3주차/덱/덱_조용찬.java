fbd7a1833d1560289a6f0652b76b2843bb704af7
yongchan <yongchan46@gmail.com>
2023-04-01 21:24:16 +0900

    3주차 덱 수정
    덱 문제 수정

--- 3주차/덱/덱_조용찬.java
+++ 3주차/덱/덱_조용찬.java
@@ -6,17 +6,17 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-public class java08_덱 {
+public class java08_덱_수정 {
 
     public static ArrayList<Integer> front = new ArrayList<>();
     public static ArrayList<Integer> back = new ArrayList<>();
-
-    public static StringBuilder sb = new StringBuilder();
 
     public static void main(String args[]) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         int N = Integer.parseInt(bf.readLine());
+
+        StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(bf.readLine(), " ");
@@ -31,11 +31,11 @@ public class java08_덱 {
             } else {
                 switch (st.nextToken()) {
                     case "pop_front": {
-                        pop_front();
+                        sb.append(pop_front()).append('\n');
                         break;
                     }
                     case "pop_back": {
-                        pop_back();
+                        sb.append(pop_back()).append('\n');
                         break;
                     }
                     case "size": {
@@ -67,27 +67,33 @@ public class java08_덱 {
         back.add(x);
     }
 
-    public static void pop_front() {
+    public static int pop_front() {
+        int num = 0;
         if (front.isEmpty() && back.isEmpty()) {
-            sb.append(-1).append('\n');
+            return -1;
         } else if (front.isEmpty() && !back.isEmpty()) {
-            sb.append(back.get(0)).append('\n');
+            num = back.get(0);
             back.remove(0);
+            return num;
         } else {
-            sb.append(front.get(front.size()-1)).append('\n');
+            num = front.get(front.size()-1);
             front.remove(front.size()-1);
+            return num;
         }
     }
 
-    public static void pop_back() {
+    public static int pop_back() {
+        int num = 0;
         if (front.isEmpty() && back.isEmpty()) {
-            sb.append(-1).append('\n');
+            return -1;
         } else if (back.isEmpty() && !front.isEmpty()) {
-            sb.append(front.get(0)).append('\n');
+            num = front.get(0);
             front.remove(0);
+            return num;
         } else {
-            sb.append(back.get(back.size()-1)).append('\n');
+            num = back.get(back.size()-1);
             back.remove(back.size()-1);
+            return num;
         }
     }
 
