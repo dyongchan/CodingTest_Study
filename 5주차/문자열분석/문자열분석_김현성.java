6bd91a28143701ad4b20993bc00ae5e27ba3a595
evgeniac10 <rlagustjd2684@gmail.com>
2023-04-15 20:21:57 +0900

    무한루프 이슈 제거
    무한루프 제거

--- 5주차/문자열분석/문자열분석_김현성.java
+++ 5주차/문자열분석/문자열분석_김현성.java
@@ -7,12 +7,15 @@ public class 백준_문자열분석_10820번 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String str;
-		int arr[] = new int[4];
 		
 		
-		while((str = br.readLine()) == null) {
+		
+		while((str = br.readLine()) != null) {
+			if(str.equals("")) break;
+			int arr[] = new int[4];
 			for(int i=0; i<str.length(); i++) {
 				char c = str.charAt(i);
+				
 				if(c >= 'a' &&c <='z') {
 					arr[0]++;
 				}else if(c>='A'&&c<='Z') {
@@ -22,12 +25,12 @@ public class 백준_문자열분석_10820번 {
 				}else if(c==' ') {
 					arr[3]++;
 				}
+				
 			}
+			for(int j=0; j<arr.length; j++) {
+				System.out.print(arr[j]+" ");
+			}
+			System.out.println("");
 		}
-		for(int j=0; j<4; j++) {
-			System.out.print(arr[j]+" ");
-		}
- 
 	}
-
 }
