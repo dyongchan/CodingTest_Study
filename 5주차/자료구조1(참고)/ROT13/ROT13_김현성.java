a88ca4a6c75168709b7629db2b115db55ba01948
evgeniac10 <rlagustjd2684@gmail.com>
2023-04-20 16:45:16 +0900

    코테 준비

--- 5주차/자료구조1(참고)/ROT13/ROT13_김현성.java
+++ 5주차/자료구조1(참고)/ROT13/ROT13_김현성.java
@@ -13,13 +13,22 @@ public class 백준_ROT13_11655번 {
 		String str = br.readLine();
 		
 		for(int i=0; i<str.length(); i++) {
-			if(str.charAt(i)== ' ') {
-				sb.append(' ');
-			}
-			else {
-				char ch = (char)(str.charAt(i)+13);	
-				sb.append(ch);
-			}
+			 if (str.charAt(i) >= 'A' && str.charAt(i) <= 'Z') {
+	                if (str.charAt(i) >= 'N') {
+	                    sb.append((char)(str.charAt(i) - 13));
+	                } else {
+	                	sb.append((char)(str.charAt(i) + 13));
+	                }
+	            } else if (str.charAt(i) >= 'a' && str.charAt(i) <= 'z') {
+	                if (str.charAt(i) >= 'n') {
+	                	sb.append((char)(str.charAt(i) - 13));
+	                } else {
+	                	sb.append((char)(str.charAt(i) + 13));
+	                }
+	            }
+	            else {
+	            	sb.append(str.charAt(i));
+	            }
 		}
 		System.out.println(sb);
 	}
